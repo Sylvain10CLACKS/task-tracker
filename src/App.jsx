e6027450c4e499b7572a024dca8dd579d87de648
Sylvain Ntirenganya <ntirenganyasy02@gmail.com>
2024-12-18 19:32:53 +0200

    --amend

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditTask from "./pages/EditTask";
-import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/tasks" element={<EditTask/>}/>
-          <Route path="*" element={<NotFound/>}/>
-        </Routes>
-      </Router>
-      
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/task/:id" element={<EditTask />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
